a6c7bb3fa0919aa706162b4291a6c079a8fe8e1d
Dan <mrpickles3rd@googlemail.com>
2020-12-05 09:56:15 +0000

    Serch box and bbutton

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Should render a search <input> with place holder text "Search"', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByPlaceholderText('Search');
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('Should render a search <button> with the text "Search"', () => {
+  render(<App />);
+  const linkElement = screen.getByText('Search.');
   expect(linkElement).toBeInTheDocument();
 });
